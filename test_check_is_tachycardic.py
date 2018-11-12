c821222a51199e27761f4c860c9f1e2c4c78f39f
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-11-12 13:54:59 -0500

    add test file for check_tachycardic

---
+++ test_check_is_tachycardic.py
@@ -0,0 +1,11 @@
+from check_is_tachycardic import check_tachycardic
+
+
+@pytest.mark.parametrize("age, heart_rate, expected", [
+    (4, 160, "tachycardic"),
+    (10, 120, "not tachycardic"),
+    (0.3, 188, "tachycardic"),
+])
+def test_check_tachycardic(age, heart_rate, expected):
+    output = check_tachycardic(age, heart_rate)
+    assert output == expected
